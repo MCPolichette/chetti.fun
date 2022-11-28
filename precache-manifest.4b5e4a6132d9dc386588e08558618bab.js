c1fbec615bd57018e40c6364841e6e3e830b1187
Mark Polichette <Mark@polichette.com>
2022-11-28 14:38:35 -0700

    Updates

---
+++ precache-manifest.4b5e4a6132d9dc386588e08558618bab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c44d0588564ce2546f02",
+    "url": "/static/css/main.07224719.chunk.css"
+  },
+  {
+    "revision": "c44d0588564ce2546f02",
+    "url": "/static/js/main.c44d0588.chunk.js"
+  },
+  {
+    "revision": "bb2d15f8b100106d30b0",
+    "url": "/static/js/1.bb2d15f8.chunk.js"
+  },
+  {
+    "revision": "51f451d9c647af255136",
+    "url": "/static/js/2.51f451d9.chunk.js"
+  },
+  {
+    "revision": "2ff3dd98650ffb28a13e",
+    "url": "/static/js/runtime~main.2ff3dd98.js"
+  },
+  {
+    "revision": "ec0eb703105cbbedfd6f20489180f2f5",
+    "url": "/static/media/389721__uminari__rolling-rocks-01.ec0eb703.wav"
+  },
+  {
+    "revision": "12199cb7ad747b7283eca13369092e6f",
+    "url": "/static/media/574445__f-m-audio__kicking-large-stones-down-shoreline-on-stony-beach-5.12199cb7.wav"
+  },
+  {
+    "revision": "6ce8dee3945c2843c2f4402d8f1962b4",
+    "url": "/static/media/574449__f-m-audio__pushing-large-stones-down-shoreline-on-stony-beach-2.6ce8dee3.wav"
+  },
+  {
+    "revision": "0d0e62da762f9ccf968a3fd365ae9307",
+    "url": "/static/media/318968__djm62__successarpeggio.0d0e62da.flac"
+  },
+  {
+    "revision": "ca15d0d9f26a13732781f76a88aacac1",
+    "url": "/static/media/419023__jacco18__acess-denied-buzz.ca15d0d9.mp3"
+  },
+  {
+    "revision": "7bd4b931960954f5b596b464d89f142a",
+    "url": "/static/media/pramod-tiwari-3UgbE1b43j8-unsplash.7bd4b931.jpg"
+  },
+  {
+    "revision": "ea095f9124d63ed0cd77d07b1cb96760",
+    "url": "/index.html"
+  }
+];
